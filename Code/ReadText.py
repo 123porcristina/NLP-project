b87946a2296cca81a09f36d83af414008568c2c4
RENZO CASTAGNINO <renzo_tp@outlook.com>
2019-10-30 17:57:31 -0400

    upload files ReadText and main. This should make the PDFs and DOCs available to read and store in a pickle file.

--- Code/ReadText.py
+++ Code/ReadText.py
@@ -1,34 +1,95 @@
-from os import listdir
-from os.path import isfile, join
-from nltk.tokenize import TreebankWordTokenizer
-import docx
-
-# nltk.download('punkt')
-# nltk.download('stopwords')
+from os import walk
+from docx import Document
+import PyPDF2
+import os
+import pandas as pd
+import re
 
 
 class ReadDoc:
-    def __init__(self, directory, type_doc):
-        self.directory = directory
-        self.type_doc = type_doc
+    def __init__(self, path):
+        self.path = path
 
     def read_directory_files(self):
-        """Get documents from the directory"""
-        file_texts = []
-        files = [f for f in listdir(self.directory) if isfile(join(self.directory, f))]
-        for f in files: #maybe here we can do a validation type doc
-            file_text = self.getTextDoc(join(self.directory, f))
-            # print(file_text)
-            file_texts.append({"file": f, "content": file_text})
-        return file_texts
+        df_files = pd.DataFrame(columns=['city', 'state', 'year', 'type_doc', 'speech'])
 
-    def getTextDoc(self, filename):
-        """Read text from documents"""
-        doc = docx.Document(filename)
-        fullText = []
+        """RC. Iterate over each folder to find possible .PDFs and .DOCX documents.also"""
+        for root, dirs, files in walk(self.path):
+            for file_ in files:
+                if file_.endswith('.docx'):
+                    doc_text = ReadDoc.get_text_doc(root, file_)
+                    city_text = re.split(', |\(', file_)[0]
+                    state_text = re.search('[A-Z]+[A-Z]', file_)[0]
+                    year_text = re.findall(r'.*([1-3][0-9]{3})', (os.path.join(root, file_)))[0]
+                    df_files = df_files.append({'city': city_text,
+                                                'state': state_text,
+                                                'year': year_text,
+                                                'type_doc': '.docx',
+                                                'speech': doc_text}, ignore_index=True)
+
+                elif file_.endswith('.pdf'):
+                    pdf_text = ReadDoc.get_text_pdf(root, file_)
+                    city_text = re.split(', |\(', file_)[0]
+                    state_text = re.search('[A-Z]+[A-Z]', file_)[0]
+                    year_text = re.findall(r'.*([1-3][0-9]{3})', (os.path.join(root, file_)))[0]
+                    df_files = df_files.append({'city': city_text,
+                                                 'state': state_text,
+                                                 'year': year_text,
+                                                 'type_doc': '.pdf',
+                                                 'speech': pdf_text}, ignore_index=True)
+
+        return df_files
+
+    """RC. This function looks for all the .DOCX documents and return just the text for each document"""
+    @staticmethod
+    def get_text_doc(root, file_):
+        doc = Document((os.path.join(root, file_)))
+        doc_text = []
         for para in doc.paragraphs:
-            fullText.append(para.text)
-        return '\n'.join(fullText)
+            doc_text.append(para.text)
+        return doc_text
 
-    def getTextPDF(self, filename):
-        pass
+    """RC. This function looks for all the .PDF documents and return just the text for each document"""
+    @staticmethod
+    def get_text_pdf(root, file_):
+        pdfFileObj = open((os.path.join(root, file_)), 'rb')
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj, strict=False, overwriteWarnings=True)
+        num_pages = pdfReader.numPages
+        count = 0
+        pdf_text = ""
+        while count < num_pages:
+            try:
+                pageObj = pdfReader.getPage(count)
+                pdf_text += pageObj.extractText()
+                count += 1
+            except Exception as e:
+                print(("BAD FILE: ",pageObj, ' - ', root, file_))
+                print("PDF failed to read. Error is: ", e)
+                return
+
+        return pdf_text
+
+
+class SaveDf:
+    def __init__(self, dir_base, df_files):
+        self.dir_base = dir_base
+        self.df_files = df_files
+
+    def save_dataframe(self):
+        data_df = pd.DataFrame(self.df_files)
+        return data_df.to_pickle(self.dir_base+'/'+'df_data.pickle')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Code/main.py
+++ Code/main.py
@@ -1,31 +1,46 @@
 from Code import ReadText
 from Code import PreprocessingSpeech as ps
+from pathlib import Path
+import pandas as pd
 
 
 def main():
-    dir_base = 'C:/Users/Giraldo/Documents/NLC/NLP/Data/Base Data'
-    dir_curr = 'C:/Users/Giraldo/Documents/NLC/NLP/Data/Current Data'
+    dir_base = (str(Path(__file__).parents[1]) + '/Data')
 
-    """Read texts"""
-    readText = ReadText.ReadDoc(directory=dir_base, type_doc='docx')
-    base_speeches = readText.read_directory_files()
-    readText = ReadText.ReadDoc(directory=dir_curr, type_doc='docx')
-    curr_speeches = readText.read_directory_files()
+    """Read PDFs and DOCs"""
+    readText = ReadText.ReadDoc(dir_base)
+    df_files = readText.read_directory_files()
+    print(df_files)
+
+    """Save the Dataframe structure locally into a pickle file to easiness of use"""
+    save_df = ReadText.SaveDf(dir_base, df_files)
+    save_df.save_dataframe()
+
+
+    """Read the pickle file"""
+    #df_file = pd.read_pickle(dir_base+"/df_data.pickle")
+    #print(df_file)
+    #print(df_file.loc[3,'speech'])
 
     """Preprocessing texts"""
-    speech = ps.Preprocessing(base_speeches=base_speeches, curr_speeches=curr_speeches)
-    tokenWord_base = speech.tokenizeWord(base_speeches)
-    tokenWord_curr = speech.tokenizeWord(curr_speeches)
+    # speech = ps.Preprocessing(base_speeches=base_speeches, curr_speeches=curr_speeches)
+    # tokenWord_base = speech.tokenizeWord(base_speeches)
+    # tokenWord_curr = speech.tokenizeWord(curr_speeches)
 
     """Get the differences"""
-    differences = speech.getDifferences(tokenWord_base, tokenWord_curr)
+    # differences = speech.getDifferences(tokenWord_base, tokenWord_curr)
 
     """Get the frequencies"""
-    doc_curr, frq_doc_base, frq_doc_curr = speech.plot(tokenWord_base, tokenWord_curr)
+    # doc_curr, frq_doc_base, frq_doc_curr = speech.plot(tokenWord_base, tokenWord_curr)
 
     """Probability distribution/high probability"""
-    mle_probab = speech.mle_distribution(doc_curr, frq_doc_base, frq_doc_curr)
-    doc_curr, freq_bi_curr, freq_bi_base, bigrams_doc, bigrams_base = speech.bigrams(tokenWord_base, tokenWord_curr)
-    high_prob_bigram = speech.mle_bigram(freq_bi_curr, freq_bi_base, bigrams_doc, len(tokenWord_curr))
+    # mle_probab = speech.mle_distribution(doc_curr, frq_doc_base, frq_doc_curr)
+    # doc_curr, freq_bi_curr, freq_bi_base, bigrams_doc, bigrams_base = speech.bigrams(tokenWord_base, tokenWord_curr)
+    # high_prob_bigram = speech.mle_bigram(freq_bi_curr, freq_bi_base, bigrams_doc, len(tokenWord_curr))
+
 
 main()
+
+
+
+
