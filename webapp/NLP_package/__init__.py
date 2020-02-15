02a9a329723dd86a6da7b1bb8fca6aaf2f93b3ad
Kanchan Ghimire <kanchan.ghimire92@gmail.com>
2020-02-15 10:51:31 -0500

    First commit

--- webapp/NLP_package/__init__.py
+++
@@ -1,9 +0,0 @@
-import dash_bootstrap_components as dbc
-from flask import Flask
-import dash
-
-server = Flask(__name__)
-server.config['DEBUG'] = True
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], server=server, url_base_pathname='/')
-
-from NLP_package import routes
